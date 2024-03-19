e1cb919d5b41e93efebb762134cababfe22a7f68
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-18 21:18:15 -0400

    Finish create form

--- client/src/pages/admin/CreateChallenge.tsx
+++ client/src/pages/admin/CreateChallenge.tsx
@@ -13,10 +13,14 @@ const CreateChallenge = () => {
 	const pointsType = useWatch("pointsType", form);
 	const challengeType = useWatch("challengeType", form);
 
+	const onFinish = (values: any) => {
+		console.log(values);
+	};
+
 	return (
 		<>
 			<h1 className="text-center">Create Challenge</h1>
-			<Form form={form} layout="vertical">
+			<Form form={form} layout="vertical" onFinish={onFinish}>
 				<Form.Item label="Title" name="title" rules={[{ required: true }]}>
 					<Input showCount maxLength={20} placeholder="Enter challenge title" />
 				</Form.Item>
@@ -118,19 +122,39 @@ const CreateChallenge = () => {
 				</Form.Item>
 
 				{challengeType === "multiple-choice" ? (
-					<Form.Item
-						label="Multiple Choice Options"
-						rules={[{ required: true }, { min: 4 }]}
-					>
-						<Form.List name="multipleChoiceOptions" initialValue={[]}>
+					<Form.Item label="Multiple Choice Options">
+						<Form.List
+							key="multipleChoiceOptions"
+							name="multipleChoiceOptions"
+							initialValue={[
+								{ name: 0, key: 0 },
+								{ name: 1, key: 1 },
+								{ name: 2, key: 2 },
+								{ name: 3, key: 3 },
+							]}
+						>
 							{(fields, { add, remove }) => (
 								<div>
 									{fields.map((field) => (
 										<div className="flex gap-2 w-full">
-											<Form.Item {...field} className="mb-2 w-full">
+											<Form.Item
+												{...field}
+												name={[field.name, "option"]}
+												className="mb-2 w-full"
+												rules={[
+													{
+														required: true,
+														message: "'option' is required",
+													},
+												]}
+											>
 												<Input placeholder="Enter option" />
 											</Form.Item>
-											<Button danger onClick={() => remove(field.name)}>
+											<Button
+												danger
+												disabled={fields.length <= 4}
+												onClick={() => remove(field.name)}
+											>
 												Remove
 											</Button>
 										</div>
@@ -143,45 +167,66 @@ const CreateChallenge = () => {
 						</Form.List>
 					</Form.Item>
 				) : challengeType === "short-answer" ? (
-					<Form.List name="shortAnswerOptions" initialValue={[]}>
-						{(fields, { add, remove }) => (
-							<div>
-								{fields.map((field) => (
-									<Card className="relative">
-										<CloseCircleOutlined
-											className="absolute right-3 top-3 text-xl text-red-400"
-											onClick={() => remove(field.name)}
-										/>
-										<Flex>
-											<Form.Item
-												{...field}
-												name="isCaseSensitive"
-												className="mb-2 w-full"
-											>
-												<Checkbox>Case Sensitive?</Checkbox>
-											</Form.Item>
-											<Form.Item
-												{...field}
-												name="isRegularExpression"
-												className="mb-2 w-full"
-											>
-												<Checkbox>Regular Expression?</Checkbox>
-											</Form.Item>
-										</Flex>
-										<Form.Item {...field} className="mb-2 w-full">
-											<Input placeholder="Enter option" />
-										</Form.Item>
-										<Button danger onClick={() => remove(field.name)}>
-											Remove
+					<Form.Item label="Short Answer Options">
+						<Form.List
+							key="shortAnswerOptions"
+							name="shortAnswerOptions"
+							initialValue={[{ name: 0, key: 0 }]}
+						>
+							{(fields, { add, remove }) => {
+								return (
+									<div className="flex flex-col gap-2">
+										{fields.map((field) => (
+											<Card className="relative">
+												{fields.length > 1 && (
+													<CloseCircleOutlined
+														className="absolute right-3 top-3 text-xl text-red-400"
+														onClick={() => remove(field.name)}
+													/>
+												)}
+												<Flex>
+													<Form.Item
+														{...field}
+														valuePropName="checked"
+														initialValue={false}
+														name={[field.name, "isCaseSensitive"]}
+														className="mb-2 w-full"
+													>
+														<Checkbox>Case Sensitive?</Checkbox>
+													</Form.Item>
+													<Form.Item
+														{...field}
+														valuePropName="checked"
+														initialValue={false}
+														name={[field.name, "isRegularExpression"]}
+														className="mb-2 w-full"
+													>
+														<Checkbox>Regular Expression?</Checkbox>
+													</Form.Item>
+												</Flex>
+												<Form.Item
+													{...field}
+													name={[field.name, "option"]}
+													className="mb-2 w-full"
+													rules={[
+														{
+															required: true,
+															message: "'option' is required",
+														},
+													]}
+												>
+													<Input placeholder="Enter option" />
+												</Form.Item>
+											</Card>
+										))}
+										<Button type="dashed" onClick={() => add()} block>
+											+ Add Option
 										</Button>
-									</Card>
-								))}
-								<Button type="dashed" onClick={() => add()} block>
-									+ Add Option
-								</Button>
-							</div>
-						)}
-					</Form.List>
+									</div>
+								);
+							}}
+						</Form.List>
+					</Form.Item>
 				) : null}
 
 				<Form.Item>
--- server/src/middleware/verifyIsAdmin.ts
+++ server/src/middleware/verifyIsAdmin.ts
@@ -2,8 +2,6 @@ import { Response, NextFunction, Request } from "express";
 import { verifyAccess } from "./verifyAccess";
 
 export function verifyIsAdmin(req: Request, res: Response, next: NextFunction) {
-	console.log(req)
-
 	if (!req.payload) {
 		verifyAccess(req, res, verifyIsAdmin.bind(null, req, res, next));
 		return;
