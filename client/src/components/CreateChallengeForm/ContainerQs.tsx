02de3b54672781c8640f343050e5c1dbbadbe4ec
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-19 00:43:45 -0400

    Create separate container component

---
+++ client/src/components/CreateChallengeForm/ContainerQs.tsx
@@ -0,0 +1,97 @@
+import { useRef } from "react";
+import FormItemWithSublabel from "../FormItemWithSubLabel";
+import { Alert, Button, Form, Input, InputNumber } from "antd";
+
+const ContainerQs = () => {
+	const nextKeyRef = useRef(1);
+
+	return (
+		<>
+			<FormItemWithSublabel
+				label="Container Image"
+				name="containerImage"
+				subLabel="Example: 'ubuntu:latest' or 'python:3.9'"
+				rules={[{ required: true }]}
+			>
+				<Input placeholder="Enter container image" />
+			</FormItemWithSublabel>
+
+			<FormItemWithSublabel
+				name="containerPorts"
+				rules={[{ required: true }]}
+				label="Container Ports"
+				subLabel="The ports that need to be exposed to the participant. Example: '80', '8080', '3000'"
+			>
+				<Form.List name="containerPorts" initialValue={[{ name: 0, key: 0 }]}>
+					{(fields, { add, remove }) => (
+						<>
+							{fields.map((field) => (
+								<div key={field.key} className="flex gap-2 w-full">
+									<Form.Item
+										name={[field.name, "port"]}
+										className="mb-2 w-full"
+										rules={[
+											{
+												required: true,
+												message: "'port' is required",
+											},
+										]}
+									>
+										<InputNumber
+											style={{ width: "100%" }}
+											placeholder="Enter port"
+										/>
+									</Form.Item>
+									<Button
+										danger
+										disabled={fields.length <= 1}
+										onClick={() => remove(field.name)}
+									>
+										Remove
+									</Button>
+								</div>
+							))}
+							<Button
+								type="dashed"
+								onClick={() => {
+									add(
+										{ name: nextKeyRef.current, key: nextKeyRef.current },
+										fields.length
+									);
+									nextKeyRef.current += 1;
+								}}
+								block
+							>
+								+ Add Port
+							</Button>
+						</>
+					)}
+				</Form.List>
+			</FormItemWithSublabel>
+
+			<FormItemWithSublabel
+				name="containerInstructions"
+				label="Container Access Instructions"
+				rules={[{ required: true }]}
+				subLabelRender={() => (
+					<Alert
+						className="my-2"
+						type="info"
+						showIcon
+						message={
+							<div
+								dangerouslySetInnerHTML={{
+									__html: "Provide instructions to the participants on how to access their container. Example: 'Visit {addr:port:8080} to access your container.' Use <b>{port:PORT}</b> to dynamically insert the port number or <b>{addr:port:PORT}</b> to dynamically insert the address with the port number.",
+								}}
+							/>
+						}
+					/>
+				)}
+			>
+				<Input placeholder="Enter instructions" />
+			</FormItemWithSublabel>
+		</>
+	);
+};
+
+export default ContainerQs;
--- client/src/pages/admin/CreateChallenge.tsx
+++ client/src/pages/admin/CreateChallenge.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Alert, Button, Checkbox, Form, Input, InputNumber, Radio } from "antd";
+import { Button, Checkbox, Form, Input, InputNumber, Radio } from "antd";
 import ReactQuill from "react-quill";
 import TextArea from "antd/es/input/TextArea";
 import { useForm, useWatch } from "antd/es/form/Form";
@@ -8,6 +8,7 @@ import StaticPointQs from "@/components/CreateChallengeForm/StaticPointQs";
 import DynamicPointQs from "@/components/CreateChallengeForm/DynamicPointQs";
 import MultipleChoiceQs from "@/components/CreateChallengeForm/MultipleChoiceQs";
 import ShortAnswerQs from "@/components/CreateChallengeForm/ShortAnswerQs";
+import ContainerQs from "@/components/CreateChallengeForm/ContainerQs";
 
 const CreateChallenge = () => {
 	const [description, setDescription] = useState("");
@@ -16,15 +17,10 @@ const CreateChallenge = () => {
 	const pointsType = useWatch("pointsType", form);
 	const challengeType = useWatch("challengeType", form);
 	const isContainer = useWatch("isContainer", form);
-	const descriptionValue = useWatch("description", form);
-
-	console.log(descriptionValue);
 
 	const onFinish = (values: any) => {
 		console.log(values);
 	};
-
-	console.log({ isContainer });
 
 	return (
 		<>
@@ -131,92 +127,7 @@ const CreateChallenge = () => {
 					<Checkbox />
 				</FormItemWithSublabel>
 
-				{isContainer ? (
-					<>
-						<FormItemWithSublabel
-							label="Container Image"
-							name="containerImage"
-							subLabel="Example: 'ubuntu:latest' or 'python:3.9'"
-							rules={[{ required: true }]}
-						>
-							<Input placeholder="Enter container image" />
-						</FormItemWithSublabel>
-
-						<FormItemWithSublabel
-							name="containerPorts"
-							rules={[{ required: true }]}
-							label="Container Ports"
-							subLabel="The ports that need to be exposed to the participant. Example: '80', '8080', '3000'"
-						>
-							<Form.List name="containerPorts" initialValue={[{ name: 0, key: 0 }]}>
-								{(fields, { add, remove }) => (
-									<>
-										{fields.map((field) => (
-											<div key={field.key} className="flex gap-2 w-full">
-												<Form.Item
-													name={[field.name, "port"]}
-													className="mb-2 w-full"
-													rules={[
-														{
-															required: true,
-															message: "'port' is required",
-														},
-													]}
-												>
-													<InputNumber
-														style={{ width: "100%" }}
-														placeholder="Enter port"
-													/>
-												</Form.Item>
-												<Button
-													danger
-													disabled={fields.length <= 1}
-													onClick={() => remove(field.name)}
-												>
-													Remove
-												</Button>
-											</div>
-										))}
-										<Button
-											type="dashed"
-											onClick={() => {
-												add(
-													{ name: fields.length, key: fields.length },
-													fields.length
-												);
-											}}
-											block
-										>
-											+ Add Port
-										</Button>
-									</>
-								)}
-							</Form.List>
-						</FormItemWithSublabel>
-
-						<FormItemWithSublabel
-							name="containerInstructions"
-							label="Container Access Instructions"
-							rules={[{ required: true }]}
-							subLabelRender={() => (
-								<Alert
-									className="my-2"
-									type="info"
-									showIcon
-									message={
-										<div
-											dangerouslySetInnerHTML={{
-												__html: "Provide instructions to the participants on how to access their container. Example: 'Visit {addr:port:8080} to access your container.' Use <b>{port:PORT}</b> to dynamically insert the port number or <b>{addr:port:PORT}</b> to dynamically insert the address with the port number.",
-											}}
-										/>
-									}
-								/>
-							)}
-						>
-							<Input></Input>
-						</FormItemWithSublabel>
-					</>
-				) : null}
+				{isContainer ? <ContainerQs /> : null}
 
 				<Form.Item>
 					<Button type="primary" htmlType="submit">
